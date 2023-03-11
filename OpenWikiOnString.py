58acdbeae3ac98db6ca6f13808283d6047e763bf
punkochel <45883647+punkochel@users.noreply.github.com>
2023-03-11 23:24:54 +0300

    refactoring and adaptation for OpenMP

--- OpenWikiOnString.py
+++
@@ -1,9 +0,0 @@
-import sublime
-import sublime_plugin
-import webbrowser
-
-
-class OpenWikiOnString(sublime_plugin.TextCommand):
-    def run(self, edit):
-        query = self.view.substr(self.view.sel()[0])
-        webbrowser.open_new("http://wiki.sa-mp.com/wiki/" + query)
